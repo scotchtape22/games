ef02e395438376dc55cd9e526ede3f75a277686b
scotchtape22 <scottwambold@gmail.com>
2020-04-02 07:15:51 -0400

    Create broker.py

---
+++ copper/broker.py
@@ -0,0 +1,10 @@
+#!/bin/python
+# Copper and Coal Broker
+# Using a save file and a list of orders
+# Returns a new save file
+# Will ideally be used with the draw functions to create the map
+
+# Weather Phase
+
+# Edict Phase
+
