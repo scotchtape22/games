9271c0981f7539191f374e1a0d6aa5eebb2540e7
scotchtape22 <scottwambold@gmail.com>
2020-06-12 09:58:11 -0400

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__wclt__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
+@app.route('/login')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/leagues')
+def hello_world():
+    return 'Hello, World!'
+    
+@app.route('/teams')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/games')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/forum')
+def hello_world():
+    return 'Hello, World!'
